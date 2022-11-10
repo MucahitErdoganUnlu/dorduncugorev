2e14c88c6801b29b72cd1a91dcc7a4fb64fb0552
MucahitErdoganUnlu <mucahit.719@hotmail.com>
2022-11-11 02:55:01 +0300

    odevin son hali

--- dorduncugorev.cpp
+++ dorduncugorev.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
+#include <string>
+
+struct Province;
+
+struct Country {
+	std::string name;
+	struct Province *capital;
+};
+
+struct Province {
+	std::string name;
+	struct Country* domain;
+};
+
 
 int main(){
-	std::cout << "Haha" << std::endl;
+	
+	struct Country tr;
+	tr.name = "Turkiye";
+	
+	struct Province ank;
+	ank.name = "Ankara";
+//	ank.domain = &tr;
+	tr.capital = &ank;
+	
+	struct Province ist;
+	ist.name = "Istanbul";
+	ist.domain = &tr;
+	
+	std::cout << (ist.domain)->name;
+	
+/*	
+	int b = 5;
+	int* a = &b;
+	b= 7;
+	std::cout << "a: " << a << " b: " << &b << std::endl;
+*/	
 }
